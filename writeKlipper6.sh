30ccb41d93ee9d62451e2946222b2889b389031f
Michael Predko <myke.predko@mimetics.ca>
2023-10-09 16:15:50 -0400

    Update writeKlipper6.sh
    To update mcu.cfg and provide it with "read" access, you MUST "sudo" chmod (I'll keep writing it down and maybe I'll remember).  
    
    Also restarted Klipper.

--- writeKlipper6.sh
+++ writeKlipper6.sh
@@ -276,9 +276,9 @@ if [[ "$can0UUIDs" == *", Application: Klipper"* ]]; then  # Have the canbus UUI
   mcuCfgReadTemp=$(mktemp)
   echo -e $mcuCfgRead > $mcuCfgReadTemp
   sudo cp -f $mcuCfgReadTemp $homeDirectory/printer_data/config/mcu.cfg
-#  chmod +r $homeDirectory/printer_data/config/mcu.cfg
+  sudo chmod +r $homeDirectory/printer_data/config/mcu.cfg
 
-#  sudo service klipper restart
+  sudo service klipper restart
 
   echoGreen "
 #######################################################
