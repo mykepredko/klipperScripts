486a749b0413c4adf997ca38ff9542e5912637cf
Michael Predko <myke.predko@mimetics.ca>
2024-01-02 21:18:46 -0500

    Update writeToolhead2.sh
    Removed Extraneous Commands & Changed Final message to fit in terminal window

--- writeToolhead2.sh
+++ writeToolhead2.sh
@@ -211,133 +211,15 @@ sudo systemctl enable headerUpdate1.service
 sudo systemctl start headerUpdate1.service
 
 echoYellow "
-################################################################################
-###                                                                          ###
-###  Press Any Key to Power Down                                             ###
-###                                                                          ###
-###  After Turning Off System Power:                                         ###
-###  1.  Disconnect the USB Cable to the Toolhead                            ###
-###  2.  Connect the CAN Cable between the Main Controller and the Toolhead  ###
-###  3.  Turn System Power On                                                ###
-###                                                                          ###
-################################################################################"
+######################################################
+###                                                ###
+###  Press Any Key to Power Down                   ###
+###                                                ###
+###  After Turning Off System Power:               ###
+###  1.  Disconnect the USB Cable to the Toolhead  ###
+###  2.  Connect the CAN Cable to the Toolhead     ###
+###  3.  Turn System Power On                      ###
+###                                                ###
+######################################################"
 read -N 1 -s
 sudo shutdown -P +0
-
-sleep 5s
-
-echoYellow "Flash Klipper firmware into main controller"
-can0UUIDs=`$homeDirectory/Katapult/scripts/flash_can.py -i can0 -q`
-klipperLeadUUID=${can0UUIDs%", Application: Klipper"*}
-klipperUUID=${klipperLeadUUID#*"UUID: "}
-echoRed "$lsDevice"
-sudo python3 $homeDirectory/Katapult/scripts/flashtool.py -u $klipperUUID -f $homeDirectory/klipper/out/klipper.bin
-
-
-sleep 5s  # Give system some time to restart and have CAN bus set up
-can0UUIDs=`$homeDirectory/Katapult/scripts/flash_can.py -i can0 -q`
-if [[ "$can0UUIDs" == *", Application: Klipper"* ]]; then  # Have the canbus UUID/Update mcu.cfg
-  echoYellow "Can update mcu.cfg"
-
-  klipperLeadUUID=${can0UUIDs%", Application: Klipper"*}
-  klipperUUID=${klipperLeadUUID#*"UUID: "}
-
-  toolheadCfgRead=`cat $homeDirectory/printer_data/config/toolhead.cfg`
-  sudo rm $homeDirectory/printer_data/config/toolhead.cfg
-  toolheadCfgRead=${toolheadCfgRead//$'@@@'/$klipperUUID}
-  toolheadCfgRead=${toolheadCfgRead//$'\n'/\\n}
-  toolheadCfgTemp=$(mktemp)
-  echo -e $toolheadCfgRead > $toolheadCfgReadTemp
-  sudo cp $toolheadCfgReadTemp $homeDirectory/printer_data/config/toolhead.cfg
-  chmod +r $homeDirectory/printer_data/config/toolhead.cfg
-else # Don't have canbus UUID/Setup for later update of toolhead.cfg
-fi
-
-
-  echoYellow "Add mcuUpdate.sh in the home directory"
-  
-  mcuUpdateContents=$(wget https://raw.githubusercontent.com/mykepredko/klipperScripts/main/mcuUpdate2.sh -O -)
-  mcuUpdateContents=${mcuUpdateContents//$'\n'/\\n}
-  mcuUpdateTemp=$(mktemp)
-  echo -e $mcuUpdateContents > $mcuUpdateTemp
-  sudo cp $mcuUpdateTemp $homeDirectory/mcuUpdate2.sh
-  sudo chmod +r $homeDirectory/mcuUpdate2.sh
-  sudo chmod +x $homeDirectory/mcuUpdate2.sh
-
-  echoYellow "Add mcuUpdate.service to systemd"
-  mcuServiceContents=$(wget https://raw.githubusercontent.com/mykepredko/klipperScripts/main/mcuUpdate2.service -O -)
-  mcuServiceContents=${mcuServiceContents//$'&&&'/$homeDirectory}
-  mcuServiceContents=${mcuServiceContents//$'\n'/\\n}
-  mcuServiceTemp=$(mktemp)
-  echo -e $mcuServiceContents > $mcuServiceTemp
-  sudo cp $mcuServiceTemp /etc/systemd/system/mcuUpdate2.service
-  sudo chmod +r /etc/systemd/system/mcuUpdate2.service
-
-  sudo systemctl daemon-reload
-  sudo systemctl enable mcuUpdate2.service
-  sudo systemctl start mcuUpdate2.service
-
-# + Add 10s Countdown to the sudo reboot so user can halt it
-  echoYellow "
-###################################
-###                             ###
-###  sudo reboot in 10 seconds  ###
-###                             ###
-###################################"
-  sleep 1s
-  echoYellow "9 Seconds"
-  sleep 1s
-  echoYellow "8 Seconds"
-  sleep 1s
-  echoYellow "7 Seconds"
-  sleep 1s
-  echoYellow "6 Seconds"
-  sleep 1s
-  echoYellow "5 Seconds"
-  sleep 1s
-  echoYellow "4 Seconds"
-  sleep 1s
-  echoYellow "3 Seconds"
-  sleep 1s
-  echoYellow "2 Seconds"
-  sleep 1s
-  echoYellow "1 Second"
-  echo "sudo reboot"  # Remove "echo" when ready to ship the code
-
-
-# + Add 10s Countdown to the sudo reboot so user can halt it
-echoYellow "
-###################################
-###                             ###
-###  sudo reboot in 10 seconds  ###
-###                             ###
-###################################"
-
-sleep 1s
-echoYellow "9 Seconds"
-
-sleep 1s
-echoYellow "8 Seconds"
-
-sleep 1s
-echoYellow "7 Seconds"
-
-sleep 1s
-echoYellow "6 Seconds"
-
-sleep 1s
-echoYellow "5 Seconds"
-
-sleep 1s
-echoYellow "4 Seconds"
-
-sleep 1s
-echoYellow "3 Seconds"
-
-sleep 1s
-echoYellow "2 Seconds"
-
-sleep 1s
-echoYellow "1 Second"
-
-echo "sudo reboot"  # Remove "echo" when ready to ship the code
